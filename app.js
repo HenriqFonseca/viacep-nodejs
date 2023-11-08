df0272ae9293253692c5dce9e039eea9f1cf2e8b
HenriqFonseca <henrique12095@gmail.com>
2023-11-08 20:40:32 -0300

    api viacep

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const app = express()
 
-
 const handlebars = require("express-handlebars").engine
 app.engine("handlebars", handlebars({defaultLayout:"main"}))
 app.set("view engine", "handlebars")
@@ -10,10 +9,6 @@ app.set("view engine", "handlebars")
 const bodyParser = require("body-parser")
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
-
-const db = require("./banco")
-
-
 
 
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app')
@@ -25,7 +20,84 @@ initializeApp({
     credential: cert(serviceAccount)
   })
 const db = getFirestore()
-const Endereco = db.collection("enderecos")
+const Endereco = db.collection("Enderecos")
+
+
+//* Rendezirar primeira página
+app.get("/", function(req, res){
+    
+  res.sendFile('./views/index.html', {root: __dirname})
+  
+})
+
+
+app.get("/endereco", async function(req, res){
+  let enderecos =[]
+  const snapshot= await Endereco.get()
+  snapshot.forEach((doc) => {
+      enderecos.push({id: doc.id, ...doc.data()})
+  });
+  res.render("endereco",{post: enderecos})
+  
+})
+
+app.get("/editar/:id", function(req, res){
+  var id = req.params.id;
+  const result = Endereco.doc(id).get()
+      .then((doc) => {
+          if (doc.exists) {
+              res.render("editar", { dados: doc.data(), id: id })
+          } else {
+              res.status(404).send("Endereco não encontrado")
+          }
+      })
+      .catch((error) => {
+          console.error("Erro ao buscar agendamento para edição: ", error)
+      });
+})
+
+
+
+app.get("/excluir/:id", async function(req, res){
+  let id = req.params.id;
+  const result = await Endereco.doc(id).delete()
+  res.redirect("/")
+})
+
+app.post("/cadastrar", function(req, res){
+  var result = Endereco.add({
+    nome: req.body.nome,
+    cep: req.body.cep,
+    logradouro: req.body.logradouro,
+    bairro: req.body.bairro,
+    localidade: req.body.localidade,
+    uf: req.body.uf,
+    descricao: req.body.descricao
+  }).then(function(){
+      console.log('Added document')
+      res.redirect('/')
+  })
+})
+
+app.post("/atualizar", function(req, res){
+  var id = req.body.id;
+
+  var result= Endereco.doc(id).update({
+      nome: req.body.nome,
+      telefone: req.body.telefone,
+      origem: req.body.origem,
+      data_contato: req.body.data_contato,
+      observacao: req.body.observacao
+  }).then(function(){
+      console.log('Atualizado com sucesso')
+      res.redirect('/')
+  })
+  
+})
+
+
+
+
 
 app.listen(8081, function(){
     console.log("Servidor ativo!")
